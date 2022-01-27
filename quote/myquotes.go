76a8ba31ef26c0281d73b3334cc2aa3cf7dd8280
lillesalt <fridalv@student.uia.no>
2022-01-27 16:17:10 +0100

    added files

---
+++ quote/myquotes.go
@@ -0,0 +1,20 @@
+package myquote
+
+import "fmt"
+import "rsc.io/quote"
+
+func Hello(){
+	fmt.Println(quote.Hello())
+}
+
+func Glass(){
+	fmt.Println(quote.Glass())
+}
+
+func Opt(){
+	fmt.Println(quote.Opt())
+}
+
+func Go(){
+	fmt.Println(quote.Go())
+}
